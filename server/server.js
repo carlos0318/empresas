074d454ff290c54bdf914cab35b87d5902ca00c6
Carlos Ortega <carlos.ortega.ast0318@gmai.com>
2022-07-13 17:56:32 -0500

    feat: add routes to delete, update, get and create a company

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ app.get("/", (req, res) => {
   res.send("Server funcionando");
 });
 
+app.use(express.json());
+app.use("/empresas", require("./routes/empresas-routes"));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
