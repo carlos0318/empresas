fb73e96531199e92571baf87c48f7ef5f2a72e40
Carlos Daniel Ortega Hernandez <carlos.ortega.ast0318@gmail.com>
2022-07-14 16:00:36 -0500

    feat: add 2 new functions
    The first one gets all the companies and sort them by the name.
    The second get a company by the id.

--- server/controllers/empresas-controllers.js
+++ server/controllers/empresas-controllers.js
@@ -1,8 +1,23 @@
 const connection = require('../models/connection');
 
 const getTiposEmpresa = async (req, res) => {
-  connection.query("SELECT * FROM tipoEmpresa", (error, result, fields) => {
+  connection.query("SELECT * FROM tipoEmpresa;", (error, result, fields) => {
     if(error) throw error;
+    res.send(result);
+  })
+}
+
+const getEmpresas = async (req, res) => {
+  connection.query("SELECT empresa.id, empresa.nombre, empresa.fechaConstitucion, tipoEmpresa.tipo FROM empresa INNER JOIN tipoEmpresa ON empresa.id_tipoEmpresa = tipoEmpresa.id ORDER BY empresa.nombre;", (error, result, fields) => {
+    if(error) throw error;
+    res.send(result);
+  })
+}
+
+const getEmpresa = async (req, res) => {
+  const { id } = req.params;
+  connection.query(`SELECT * FROM empresa WHERE id = ${id};`, (error, result, fields) => {
+    if(error) res.send("Algo salio mal");
     res.send(result);
   })
 }
@@ -15,7 +30,7 @@ const createEmpresa = async (req, res) => {
     : `INSERT INTO empresa(nombre, fechaConstitucion, id_tipoEmpresa) VALUES ('${nombre}', '${fechaConstitucion}', '${id_tipoEmpresa}')`;
 
   await connection.query(sql, (error, result) => {
-    if(error) throw error;
+    if(error) res.send("Algo salio mal");
     res.send('Se creo empresa');
   })
 }
@@ -23,21 +38,22 @@ const createEmpresa = async (req, res) => {
 const getEmpresaPorNombre = async (req, res) => {
   const { nombre } = req.params;
 
-  await connection.query(`SELECT * FROM empresa WHERE nombre regexp '^${nombre}'`, (error, result, fields) => {
+  await connection.query(`SELECT empresa.id, empresa.nombre, empresa.fechaConstitucion, tipoEmpresa.tipo FROM empresa INNER JOIN tipoEmpresa ON empresa.id_tipoEmpresa = tipoEmpresa.id WHERE empresa.nombre regexp '^${nombre}';`, (error, result, fields) => {
     if(error) throw error;
     res.send(result);
   });
 }
 
 const updateEmpresa = async (req, res) => {
-  const { nombre, fechaConstitucion, comentarios, id_tipoEmpresa, id } = req.body;
+  const { id } = req.params;
+  const { nombre, fechaConstitucion, comentarios, id_tipoEmpresa } = req.body;
 
   let sql = comentarios 
     ? `UPDATE empresa SET nombre = '${nombre}', fechaConstitucion = '${fechaConstitucion}', comentarios = '${comentarios}', id_tipoEmpresa = ${id_tipoEmpresa} WHERE id = ${id}`
     :`UPDATE empresa SET nombre = '${nombre}', fechaConstitucion = '${fechaConstitucion}', comentarios = ${comentarios}, id_tipoEmpresa = ${id_tipoEmpresa} WHERE id = ${id}`;
 
   connection.query(sql, (error, result) => {
-    if(error) throw error;
+    if(error) res.send("Algo salio mal");
     res.send('Se actualizo la empresa');
   })
 }
@@ -56,5 +72,7 @@ module.exports = {
   createEmpresa,
   getEmpresaPorNombre,
   updateEmpresa,
-  deleteEmpresa
+  deleteEmpresa,
+  getEmpresas,
+  getEmpresa
 }
