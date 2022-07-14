384b88b6391b4d4552244db76aa0f67a71ffe1bd
Carlos Ortega <carlos.ortega.ast0318@gmai.com>
2022-07-13 19:14:46 -0500

    feat: import and use cors to the project

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 require('dotenv').config();
 const app = express();
+const cors = require('cors');
 require('./models/connection');
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server funcionando");
